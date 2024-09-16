7037eb367c81eeee06dc87f68c4de96e5d50ec8f
Alan Alves <125587710+AlanAlves0@users.noreply.github.com>
2024-09-16 14:08:00 -0300

    adicionando a api do whatsApp

---
+++ apps/api.js
@@ -0,0 +1,25 @@
+const form = document.getElementById("form");
+form.addEventListener('submit', function(event)  {
+    event.preventDefault();
+
+    const formData = new FormData(form)
+    console.log(formData)
+
+    const nome = document.querySelector('#nome')
+    const email = document.querySelector('#email')
+    const telefone = document.querySelector('#telefone')
+    const servico = document.querySelector('#servico')
+    const ajudar = document.querySelector('#ajudar')
+    
+
+    const formularioValor = `Nome: ${nome.value} | Email: ${email.value} | Telefone: ${telefone.value} | Assunto: ${servico.value} | Descrição: ${ajudar.value} `
+
+    const message = encodeURIComponent(formularioValor)
+    const phone = ""
+
+    window.open(`https://wa.me/${phone}?text=${message}`, "_blank")
+})
+
+
+
+
